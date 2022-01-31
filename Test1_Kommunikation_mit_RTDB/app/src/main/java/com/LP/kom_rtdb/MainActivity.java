88036ed0f3219158dd713220acbf50283bb71450
ehk45 <94115483+ehk45@users.noreply.github.com>
2022-01-31 14:29:21 +0100

    App empfängt Topic Messages
    App kann Messages, wenn App im Hintergrund läuft empfangen, durch ein Abo des Topics /topics/ring

--- Test1_Kommunikation_mit_RTDB/app/src/main/java/com/LP/kom_rtdb/MainActivity.java
+++ Test1_Kommunikation_mit_RTDB/app/src/main/java/com/LP/kom_rtdb/MainActivity.java
@@ -1,13 +1,17 @@
 package com.LP.kom_rtdb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -15,6 +19,8 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import java.time.LocalTime;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,13 @@ public class MainActivity extends AppCompatActivity {
 
          */
 
+        // subscribe the topic of ring
+        FirebaseMessaging.getInstance().subscribeToTopic("/topics/ring");
+
         TextView mytf = findViewById(R.id.tokendp);
         mytf.setText(FirebaseMessaging.getInstance().getToken().toString());
+
+
 
     }
 
@@ -39,10 +50,12 @@ public class MainActivity extends AppCompatActivity {
         int a = LocalTime.now().getSecond();
         String s = java.lang.String.valueOf(a);
         myRef.setValue(s);
-
-
+        
+        FirebaseMessaging.getInstance().subscribeToTopic("/topics/ring");
 
     }
+
+
 
 
 
