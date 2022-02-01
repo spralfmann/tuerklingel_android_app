06b2047bdb5cf3a5e96103be4d5887936a1d3303
ehk45 <94115483+ehk45@users.noreply.github.com>
2022-02-01 21:59:34 +0100

    [N.A.] Testbild laden von Storage
    noch nicht Funktionstüchtig
    umgebung steht, bild wird noch nicht geladen

--- Test1_Kommunikation_mit_RTDB/app/src/main/java/com/LP/kom_rtdb/MainActivity.java
+++ Test1_Kommunikation_mit_RTDB/app/src/main/java/com/LP/kom_rtdb/MainActivity.java
@@ -4,15 +4,25 @@ package com.LP.kom_rtdb;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.time.LocalTime;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
 
     private static final String TAG = "MainActivity";
 
@@ -20,20 +30,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        Button knopf = findViewById(R.id.bt_sende);
-        knopf.setOnClickListener((View.OnClickListener) this);
 
-         */
 
         // Abonniert den Topic, für das Empfangen der Benachrichtigungen der Türklingel
         FirebaseMessaging.getInstance().subscribeToTopic("/topics/ring");
 
-        // Zeigt den aktuellen Device Token für den Firebase Messaging Service an
-        TextView mytf = findViewById(R.id.tokendp);
-        mytf.setText(FirebaseMessaging.getInstance().getToken().toString());
+
     }
 
+    public void click_download(View v){
+
+        ImageView imageView = findViewById(R.id.image);
+
+        StorageReference storageReference = storage.getReferenceFromUrl("gs://tuerklingel-a0ba8.appspot.com/pictures/Testfoto.png");
+
+        Glide.with(this /* context */)
+                .load(storageReference)
+                .into(imageView);
+
+    }
+
+
+    // Button zum Schreiben in die Firebase Realtime Database
     public void lick(View v){
 
         // Schreibt in die Firebase RTDB
